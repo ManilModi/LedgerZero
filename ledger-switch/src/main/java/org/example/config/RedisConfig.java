0e34ecfb9fa1b6dbd0973467e3091b74d4e9d75b
DEVAN CHAUHAN <155709255+Devan019@users.noreply.github.com>
2026-01-29 23:39:59 +0530

    [fix] redis

--- ledger-switch/src/main/java/org/example/config/RedisConfig.java
+++ ledger-switch/src/main/java/org/example/config/RedisConfig.java
@@ -1,10 +1,8 @@
 package org.example.config;
 
-import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
 
